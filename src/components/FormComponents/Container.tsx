373e1d0245e0f4df23323cbf9f4ede6b9b34e4fa
ukoricpaw <phero8121324@gmail.com>
2023-08-22 21:24:55 +0600

    feat: the registration form has been brought to a more presentable state

--- src/components/FormComponents/Container.tsx
+++ src/components/FormComponents/Container.tsx
@@ -3,17 +3,23 @@ import styles from '../../styles/Form.module.scss';
 
 interface ContainerIProps {
   children: ReactNode;
-  display?: 'flex' | 'column';
+  display?: 'row' | 'column';
+  spaceBetween?: boolean;
+  alignItems?: 'flex-start' | 'center';
 }
 
-export default function Container({ children, display }: ContainerIProps) {
+export default function Container({ children, display, spaceBetween, alignItems }: ContainerIProps) {
   return (
     <div className={`${styles.form__formContainer} settingsContainer`}>
       {children}
       <style jsx>
         {`
           .settingsContainer {
-            ${display && `flex-direction: ${display}; align-items: flex-start; gap: 12px`}
+            ${display &&
+            `flex-direction: ${display}; align-items: ${alignItems ?? 'flex-start'};
+            ${spaceBetween ? 'justify-content: space-between; gap: 0;' : 'gap: 12px;'}
+
+            `}
           }
         `}
       </style>
--- src/components/FormComponents/FormGroup.tsx
+++ src/components/FormComponents/FormGroup.tsx
@@ -3,11 +3,12 @@ import styles from '../../styles/Form.module.scss';
 
 interface FormGroupIProps extends FormHTMLAttributes<HTMLFormElement> {
   gap?: string;
+  width?: string;
 }
 
-export default function FormGroup({ children, gap }: FormGroupIProps) {
+export default function FormGroup({ children, gap, width }: FormGroupIProps) {
   return (
-    <form style={gap ? { gap: gap } : {}} className={styles.form}>
+    <form style={{ gap: gap ?? '', width: width ?? 'initial' }} className={styles.form}>
       {children}
     </form>
   );
